d5727187d6ef87e2bf103cf327db0855723c5d21
Kirill Firsov <kifirigor@gmail.com>
2019-03-20 19:05:35 +0300

    Add files via upload

--- AirCombar/src/main/java/Scene.java
+++ AirCombar/src/main/java/Scene.java
@@ -1,42 +1,54 @@
-import Entities.DynamicEntity;
-import org.apache.commons.math3.linear.ArrayRealVector;
-import org.apache.commons.math3.linear.RealVector;
+import entities.DynamicEntity;
+import entities.DynamicEntityInterface;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.exception.MaxCountExceededException;
+import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Scene {
-    private RealVector fullGetRightVector;
-    private List<DynamicEntity> entities = new ArrayList<DynamicEntity>();
+public class Scene implements FirstOrderDifferentialEquations {
+    private List<DynamicEntityInterface> entities = new ArrayList<DynamicEntityInterface>();
+    private double[] initialState;
+    private static int DIMENSION = 7;  // dimension of each entity
 
     public Scene() {
-        fullGetRightVector = new ArrayRealVector(
-                new double[0], false);
     }
+
+    public Scene(DynamicEntityInterface[] entity) { entities.addAll(Arrays.asList(entity)); }
 
     public void addEntity(DynamicEntity entity) {
         entities.add(entity);
 
-        fullGetRightVector.append(entity.getRight());
+
     }
 
-    public RealVector getFullGetRightVector() {
-        return fullGetRightVector;
-    }
-
-    public void setFullGetRightVector(RealVector fullGetRightVector) {
-        this.fullGetRightVector = fullGetRightVector;
-    }
-
-    public List<DynamicEntity> getEntities() {
+    public List<DynamicEntityInterface> getEntities() {
         return entities;
     }
 
-    public void setEntities(List<DynamicEntity> entities) {
-        this.entities = entities;
+    public int getDimension() {  // full dimension including all entities
+        return entities.size() * DIMENSION;
     }
 
-    public void addToGetRightVector() {
+    public double[] getInitialState() {
+        initialState = new double[getDimension()];
+        for (int j = 0; j < entities.size(); j++) {
+            for (int i = 0; i < DIMENSION; i++) {
+                initialState[i + (j*DIMENSION)] = entities.get(j).getInitialState(i);
+            }
+        }
+        return initialState;
+    }
 
+    public void computeDerivatives(double t, double[] x, double[] xDot) throws MaxCountExceededException, DimensionMismatchException {
+        for (int j = 0; j < entities.size(); j++) {
+            /*for (int i = 0; i < DIMENSION; i++) {
+                xDot[i + (j * DIMENSION)] = entities.get(j).getRight(x, xDot, i, j);
+            }*/
+
+            entities.get(j).setX(xDot, j);
+        }
     }
 }
--- AirCombar/src/main/java/test/TestApplication.java
+++ AirCombar/src/main/java/test/TestApplication.java
@@ -12,8 +12,8 @@ public class TestApplication {
     public static void main(String[] args) {
       FirstOrderIntegrator dp54 = new DormandPrince54Integrator(
               1.0e-8, 100.0, 1.0-10, 1.0e-10);
-      Aircraft aircraft = new Aircraft(new double[] {130, 241, 325, 23, 45,125});
-      Missile missile = new Missile(new double[] {124, 213, 451, 2, 5, 34});
+      Aircraft aircraft = new Aircraft(new double[] {130, 241, 325, 23, 45,125, 100});
+      Missile missile = new Missile(new double[] {124, 213, 451, 2, 5, 34, 100});
       FirstOrderDifferentialEquations scene = new Scene(new DynamicEntityInterface[] {aircraft, missile});
       double[] y = ((Scene) scene).getInitialState();
       dp54.integrate(scene, 0.0, y, 100, y);
